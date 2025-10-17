f4a0b4174284976749bf7d6cb3b487888dac4ab7
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-10-17 18:52:49 -0300

    Esculpindo os modelos e ajustando as rotas

--- volley_app/app/models.py
+++ volley_app/app/models.py
@@ -1,1 +1,31 @@
+from pydantic import BaseModel
 
+# Classe referente aos jogadores
+class Jogador(BaseModel):
+    id_number: int
+    nome: str
+    email: str
+    sexo: str
+    idade: int
+    categoria: str['Novato', 'Amador', 'Profissional'] | None = None
+
+
+# Classe referente as partidas (de vôlei)
+class Partida(BaseModel):
+    id_number: int
+    id_jogador_organizador: int
+    local: str
+    data: str
+    hora: str
+    categoria: str
+    tipo: str
+    status: bool
+
+
+# Classe referente as avaliações para as partidas e/ou aos jogadores que estiveram nela
+class Avaliacao(BaseModel):
+    id_avaliador: int
+    id_avaliado: int
+    id_partida: int
+    nota: int
+    comentario: str
--- volley_app/app/routes.py
+++ volley_app/app/routes.py
@@ -5,55 +5,55 @@ manager = FastAPI(title="VolleyballManager")
 
 # Endpoints destinados à administração dos jogadores no sistema.
 
-@manager.get("/jogadores")
+@manager.get("/jogador")
 def get_jogador():
     # Descrição - Essa função cria um novo perfil (ID) para um jogador(a).
     pass
 
-@manager.get("/jogadores/{id}")
+@manager.get("/jogador/{id}")
 def get_id_jogador():
     # Descrição - Essa função busca o perfi de um jogador(a) com base no ID existente no sistema.
     pass
 
-@manager.put("/jogadores/{id}")
+@manager.put("/jogador/{id}")
 def put_id_jogador():
     # Descrição - Essa função atualiza os dados de um jogador(a) com base no ID existente no sistema.
     pass
 
 # Endpoints destinados à administração das partidas no sistema.
 
-@manager.post("/partidas")
+@manager.post("/partida")
 def post_partida():
     # Descrição - Essa função cria uma nova partida (de vôlei em questão) no sistema.
     pass
 
-@manager.get("/partidas")
+@manager.get("/partida")
 def get_partida():
     # Descrição - Essa função lista todas as partidas (de vôlei) existentes no sistema.
     pass
 
-@manager.get("/partidas/{id}")
+@manager.get("/partida/{id}")
 def get_id_partida():
     # Descrição - Essa função exibe os detalhes de uma partida (de vôlei) existente no sistema.
     pass
 
-@manager.put("/partidas/{id}")
+@manager.put("/partida/{id}")
 def put_id_partida():
     # Descrição - Essa função atualiza os dados de uma partida (de vôlei) existente no sistema.
     pass
 
-@manager.post("/partidas/{id}/adesao")
+@manager.post("/partida/{id}/adesao")
 def post_aderir_id_partida():
     # Descrição - Essa função emite um pedido de adesão de um(a) jogador(a) à uma partida (de vôlei) existente no sistema.
     pass
 
-@manager.put("/partidas/{id}/adesao/{id}")
+@manager.put("/partida/{id}/adesao/{id}")
 def put_aderir_id_partida():
     # Descrição - Essa função emite a resposta do(a) jogador(a) organizador(a) de uma partida (de vôlei)
     # sobre o pedido de adesão de um jogador(a).
     pass
 
-@manager.post("/partidas/{id}/desistencia")
+@manager.post("/partida/{id}/desistencia")
 def post_desistir_id_partida():
     # Descrição - Essa função registra a desistência de um jogador(a) de uma partida (de vôlei) existente no sistema.
     pass
